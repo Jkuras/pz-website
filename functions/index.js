842eccde3a4ddc6bf2fac59507f19746215179e9
jkuras <j.l.kuras95@gmail.com>
2018-05-07 15:57:32 -0700

    express and cors added

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,19 @@
 const functions = require('firebase-functions');
+const cors = require('cors')
+const express = require('express');
+
+const app = express()
+app.use(cors({origin: true}))
+app.get("*", (request, response) => {
+  response.send("Hello from Express on Firebase!")
+})
+
+const api = functions.https.onRequest(app)
+
+module.exports = {
+  api
+}
+
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
